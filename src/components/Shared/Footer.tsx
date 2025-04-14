189dd3477b6f33a5f9e27cb48565802874d1983c
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-04-14 09:46:06 -0700

    updated code

--- src/components/Shared/Footer.tsx
+++ src/components/Shared/Footer.tsx
@@ -4,6 +4,10 @@ const Footer:React.FC = () => {
     return ( 
         <div className='h-screen'> 
             <p> footer page </p>
+            <p> footer page </p>
+            <p> footer page </p>
+            <p> footer page </p>
+            <p> footer page </p>
             
         </div>
     );
