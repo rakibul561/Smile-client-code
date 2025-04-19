89094aa96c5b272e8aca46ba60d8d501e4bc9019
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-04-19 09:24:56 -0700

    updated code

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -65,7 +65,7 @@ const LoginSignup = () => {
               </li>
             ))}
           </ul>
-
+            {/* style korte hbe   */}
           <button className="bg-red-500 text-white px-6 py-2 mt-6">CREATE ACCOUNT</button>
         </div>
       </div>
