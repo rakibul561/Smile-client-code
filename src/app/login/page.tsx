cfe6797661df243da46681472fc160f45cfd314b
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-04-25 10:13:55 -0700

    change code

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -76,6 +76,12 @@ const LoginSignup = () => {
           <button className="bg-red-500 text-white px-6 py-2 mt-6">CREATE ACCOUNT</button>
           <button className="bg-red-500 text-white px-6 py-2 mt-6">CREATE ACCOUNT</button>
           <button className="bg-red-500 text-white px-6 py-2 mt-6">CREATE ACCOUNT</button>
+          <button className="bg-red-500 text-white px-6 py-2 mt-6">CREATE ACCOUNT</button>
+          <button className="bg-red-500 text-white px-6 py-2 mt-6">CREATE ACCOUNT</button>
+          <button className="bg-red-500 text-white px-6 py-2 mt-6">CREATE ACCOUNT</button>
+          <button className="bg-red-500 text-white px-6 py-2 mt-6">CREATE ACCOUNT</button>
+          <button className="bg-red-500 text-white px-6 py-2 mt-6">CREATE ACCOUNT</button>
+          <button className="bg-red-500 text-white px-6 py-2 mt-6">CREATE ACCOUNT</button>
           
       </div>
     </div>
