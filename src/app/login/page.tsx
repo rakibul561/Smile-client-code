8361bb5b2a8bf7b6a88870c4515701db969a9485
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-05-09 08:44:57 -0700

    change this code

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -71,8 +71,11 @@ const LoginSignup = () => {
             <button className="bg-green-500 text-white px-6 py-2 mt-6">CREATE ACCOUNT</button>
             <button className="bg-green-500 text-white px-6 py-2 mt-6">CREATE ACCOUNT</button>
             <button className="bg-green-500 text-white px-6 py-2 mt-6">CREATE ACCOUNT</button>
-            <button className="bg-green-500 text-white px-6 py-2 mt-6">CREATE ACCOUNT</button>
 
+            <button className="bg-green-500 text-white px-6 py-2 mt-6">CREATE ACCOUNT</button>
+            <button className="bg-green-500 text-white px-6 py-2 mt-6">CREATE ACCOUNT</button>
+            <button className="bg-green-500 text-white px-6 py-2 mt-6">CREATE ACCOUNT</button>
+            <button className="bg-green-500 text-white px-6 py-2 mt-6">CREATE ACCOUNT</button>
         </div>
         
          
