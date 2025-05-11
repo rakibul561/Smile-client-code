3ddca928412f176edfa8932b704b11174a7ebd2f
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-05-11 08:57:48 -0700

    change this code

--- src/components/Shared/Footer.tsx
+++ src/components/Shared/Footer.tsx
@@ -79,6 +79,8 @@ const Footer:React.FC = () => {
           <img src="https://i.ibb.co.com/prnKtwB2/download.png " alt="Visa" className="h-6" />
           <img src="https://i.ibb.co.com/prnKtwB2/download.png " alt="Visa" className="h-6" />
           <img src="https://i.ibb.co.com/prnKtwB2/download.png " alt="Visa" className="h-6" />
+          <img src="https://i.ibb.co.com/prnKtwB2/download.png " alt="Visa" className="h-6" />
+          <img src="https://i.ibb.co.com/prnKtwB2/download.png " alt="Visa" className="h-6" />
         </div>
       </div>
     </footer> 
