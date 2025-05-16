394a08d7e5963d45bc16bcce92d5e36b005d1ad6
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-05-16 08:31:45 -0700

    updated

--- src/components/Shared/Footer.tsx
+++ src/components/Shared/Footer.tsx
@@ -100,6 +100,9 @@ const Footer:React.FC = () => {
           <img src="https://i.ibb.co.com/JW3RN64p/download-1.jpg" alt="American Express" className="h-6" />
           <img src="https://i.ibb.co.com/JW3RN64p/download-1.jpg" alt="American Express" className="h-6" />
           <img src="https://i.ibb.co.com/JW3RN64p/download-1.jpg" alt="American Express" className="h-6" />
+          <img src="https://i.ibb.co.com/JW3RN64p/download-1.jpg" alt="American Express" className="h-6" />
+          <img src="https://i.ibb.co.com/JW3RN64p/download-1.jpg" alt="American Express" className="h-6" />
+          <img src="https://i.ibb.co.com/JW3RN64p/download-1.jpg" alt="American Express" className="h-6" />
      
         </div>
       </div>
