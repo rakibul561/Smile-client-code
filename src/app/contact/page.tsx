f7d667fb478b8fe3c9ab473242d5c2f87fc52293
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-06-16 10:29:41 -0700

    contact page updated

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const page:React.FC = () => {
     return (
-        <div>
+        <div className='max-w-7xl mx-auto  bg-sky-500 text-white text-center p-12 mt-4'>
             <h2> this is a contact page </h2>
            <h1> any help for please contact our team members </h1>
             
